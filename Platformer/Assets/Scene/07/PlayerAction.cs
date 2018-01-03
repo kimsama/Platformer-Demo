3e8d7203bade95997154d7402237fc27dafbc3f5
kimsama <hwpeterkim@gmail.com>
2018-01-03 16:48:35 +0900

    added star item.

---
+++ Platformer/Assets/Scene/07/PlayerAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public NumberRenderer numberRenderer;
+
+    public int numStar;
+
+    public void Start()
+    {
+        numStar = 0;
+        numberRenderer.RenderNumber(numStar);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "star")
+        {
+            other.gameObject.SetActive(false);
+            numStar += 1;
+            numberRenderer.RenderNumber(numStar);
+        }
+    }
+
+}
