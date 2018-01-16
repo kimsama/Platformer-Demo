992431601dfcecfec7e2dc5bff2064c896f8fb0d
kimsama <hwpeterkim@gmail.com>
2018-01-16 22:37:46 +0900

    updated.

--- Platformer/Assets/Scene/07/PlayerAction.cs
+++ Platformer/Assets/Scene/07/PlayerAction.cs
@@ -13,11 +13,13 @@ public class PlayerAction : MonoBehaviour
         numStar = 0;
         numberRenderer.RenderNumber(numStar);
     }
+
+    // 충돌을 검사
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "star")
         {
-            other.gameObject.SetActive(false);
+            other.gameObject.SetActive(false); 
             numStar += 1;
             numberRenderer.RenderNumber(numStar);
         }
